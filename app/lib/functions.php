0e105f1b85463aa238a77af83c9fd16d9b67def4
Marcel Hecko <marcel.hecko@moirelabs.com>
2017-02-09 11:06:01 +0000

    updated to work without database access - generate output for accounting

--- app/lib/functions.php
+++ app/lib/functions.php
@@ -1,16 +1,16 @@
 <?php
 
-function slsp_logout($c,$cfile,$log) {
-  $log->logInfo('-- Logging out');
+function slsp_logout($c,$cfile) {
+  echo("-- Logging out\n");
   curl_setopt($c, CURLOPT_URL, "https://ib.slsp.sk/ebanking/logout/ibxlogoutyes.xml");
   curl_setopt ($c, CURLOPT_COOKIEFILE, $cfile);
   curl_setopt ($c, CURLOPT_RETURNTRANSFER, true);
   $output = curl_exec ($c);
 
   if (preg_match("<logout/>",$output)) {
-    $log->logInfo('Logout successful');
+    echo("Logout successful\n");
   } else {
-    $log->logInfo('Logout failed!');
+    echo("Logout failed!\n");
     die;
   }
 }
--- app/sync.php
+++ app/sync.php
@@ -5,9 +5,13 @@ echo("----------new sync----------\n");
 require('lib/functions.php');
 #require('db.php');
 require('config.php');
+include('config_local.php');
 
-//DataBanking SLSP zvladne naraz vyexportovat len 200 pohybov na ucte
-$datum_od=date("j.m.Y",time()-(3600*24*10));
+$month = $argv[1];
+
+// DataBanking SLSP zvladne naraz vyexportovat len 200 pohybov na ucte
+$datum_od = date("1." . $month . ".Y",time());
+
 echo("Datum od: $datum_od\n");
 
 //create a cookie file
@@ -32,6 +36,7 @@ if (!$out = curl_exec($c)) {
 echo("Returned data from HTTP request:\n");
 echo($out);
 print_r($out);
+echo("\n");
 
 curl_setopt ($c, CURLOPT_COOKIEFILE, $cfile);
 curl_setopt ($c, CURLOPT_RETURNTRANSFER, true);
@@ -50,6 +55,7 @@ $output = curl_exec ($c);
 
 echo("-- Login output from CURL:\n");
 print_r($output);
+echo("\n");
 
 echo("-- Loading XML data...\n");
 $xml = simplexml_load_string($output);
@@ -80,29 +86,16 @@ foreach ($xml->result->{'reply-account-list'}->children() as $account) {
   $i++;
 }
 
-echo('=== Ideme cyklovat - pre kazdy ucet vyberat tranzakcie');
+echo("=== Ideme cyklovat - pre kazdy ucet vyberat tranzakcie\n");
 
 foreach ($acc as $a) {
 
-  $log->logInfo('-- Vlozenie info o ucte do databazy');
-  $query = "INSERT INTO accounts (iban,name,disp_balance_eur,account_prefix,account_number,currency) VALUES ('"
-    .mysql_real_escape_string($a['account_iban'])."','"
-    .mysql_real_escape_string($a['account_name'])." (".mysql_real_escape_string($a['account_name_www']).")','"
-    .mysql_real_escape_string($a['account_disp_balance_eur'])."','"
-    .mysql_real_escape_string($a['account_prefix'])."','"
-    .mysql_real_escape_string($a['account_number'])."','"
-    .mysql_real_escape_string($a['account_currency'])."'"
-    .") ON DUPLICATE KEY UPDATE name='"
-    .mysql_real_escape_string($a['account_name'])." (".mysql_real_escape_string($a['account_name_www']).")',"
-    ." disp_balance_eur='".mysql_real_escape_string($a['account_disp_balance_eur'])."',"
-    ." account_prefix='".mysql_real_escape_string($a['account_prefix'])."',"
-    ." account_number='".mysql_real_escape_string($a['account_number'])."',"
-    ." currency='".mysql_real_escape_string($a['account_currency'])."'";
+  echo("-- Info o ucte\n");
+  print_r($a);
+  echo("\n");
 
-  mysql_query($query);
-
-  $log->logInfo("-- Nasleduje vyber uctu...");
-  $log->logInfo("Ucet: ".$a['account_uid']." - IBAN:".$a['account_iban']);
+  echo("-- Nasleduje vyber uctu...\n");
+  echo("Ucet: ".$a['account_uid']." - IBAN: ".$a['account_iban'] . "\n");
 
   $data = array('uid' => $a['account_uid'],'utyp' => $a['account_utyp'],'ucis' => $a['account_ucis'],'uprcis' => $a['account_uprcis']);
   $post = http_build_query($data, '', '&');
@@ -114,13 +107,13 @@ foreach ($acc as $a) {
   $output = curl_exec ($c);
 
   if (preg_match("<ok/>",$output)) {
-    $log->logInfo('OK');
+    echo("OK\n");
   } else {
-    $log->logInfo('FAIL');
+    echo("FAIL\n");
     die;
   }
 
-  $log->logInfo('-- Vylistovanie obratov...');
+  echo("-- Vylistovanie obratov...\n");
 
   $data = array(
     'no_f_no_od' => $datum_od, //datum od
@@ -151,46 +144,39 @@ foreach ($acc as $a) {
     $turnovers[$i]['description'] = (string) $t->{'description'};
     $turnovers[$i]['constant-symb'] = (string) $t->{'constant-symb'};
     $turnovers[$i]['currency'] = (string) $t->{'currency'};
+    $turnovers[$i]['counter-name'] = (string) $t->{'counter-name'};
+    $turnovers[$i]['counter-bank'] = (string) $t->{'counter-bank'};
+    $turnovers[$i]['counter-account'] = (string) $t->{'counter-account'};
+    $turnovers[$i]['message1'] = (string) $t->{'message1'};
+    $turnovers[$i]['message2'] = (string) $t->{'message2'};
+    $turnovers[$i]['variable-symb'] = (string) $t->{'variable-symb'};
     $turnovers[$i]['maturity-date'] = (string) date("Y-m-d",strtotime($t->{'maturity-date'}));
     $i++;
   }
 
-  $log->logInfo('Number of all turnovers: '.count($turnovers));
+  echo('Number of all turnovers: ' . count($turnovers) . "\n");
 
-  $new_turnovers=0;
   foreach ($turnovers as $t) {
-    $query = "INSERT INTO turnovers (iban,transid,amount,note,description,maturitydate,currency,constantsymb) VALUES ('".
-      mysql_real_escape_string($a['account_iban'])."','".
-      mysql_real_escape_string($t['transid'])."','".
-      mysql_real_escape_string($t['amount'])."','".
-      mysql_real_escape_string($t['note'])."','".
-      mysql_real_escape_string($t['description'])."','".
-      mysql_real_escape_string($t['maturity-date'])."','".
-      mysql_real_escape_string($t['currency'])."','".
-      mysql_real_escape_string($t['constant-symb'])."')";
-    if (mysql_query($query)) {
-      $new_turnovers++;
-    } else {
-    };
+    # print_r($t);
+    # echo("\n");
+    echo($t['maturity-date'] . "   " . $t['amount'] . " " . $t['currency'] . "\n");
+    echo("   Var symbol: " . $t['variable-symb'] . "\n");
+    echo("   Poznamka:   " . $t['note'] . "\n");
+    echo("   Popis:      " . $t['description'] . "\n");
+    echo("   Protiucet:  " . $t['counter-name'] . "   (" . $t['counter-account'] . "/" . $t['counter-bank'] . ")\n");
+    echo("   Zaznam 1:   " . $t['message1'] . "\n");
+    echo("   Zaznam 2:   " . $t['message2'] . "\n");
+    echo("\n");
   }
-  $log->logInfo('Number of new turnovers: '.$new_turnovers);
 
-  unset($turnovers,$new_turnovers);
+  unset($turnovers);
 
 }
-$log->logInfo('=== Koniec cyklu zbierania dat z uctov.');
+echo("=== Koniec cyklu zbierania dat z uctov.\n");
 
-slsp_logout($c,$cfile,$log);
+slsp_logout($c,$cfile);
 
 //vymazanie zaznamov z databazy ktore su starsie ako 5 dni
 
-$query="DELETE FROM turnovers WHERE maturitydate<'".date("Y-m-d",time()-(3600*24*5))."'"; 
-$log->logInfo('Database cleanup... '.$query);
-if (!mysql_query($query)) {
-  $log->logError('Deleting old information fro DB failed.');
-} else {
-  $log->logInfo('Deleted information from database (number removed): '.mysql_affected_rows());
-}
-
-$log->logInfo('-- End of synchronization');
+echo("-- End of synchronization\n");
 ?>
